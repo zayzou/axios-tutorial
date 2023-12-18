52b15ac63f49a10f2ab90b302e2f82708f7d5c1a
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-17 19:50:07 -0500

    4-global-instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 
-const productsUrl = 'https://course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = "https://course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const resp = await axios(productsUrl);
+      console.log(resp.data);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
