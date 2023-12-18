28ab67638a88d0d6656acdb4b55e89e91ffb31d6
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-17 19:08:47 -0500

    Get request

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
-
+const url = "https://course-api.com/react-store-products1";
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
